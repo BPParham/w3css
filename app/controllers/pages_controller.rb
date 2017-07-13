6310134e8e8fbb7ed1b93d0f29b4d241af6f514b
bobbypparham <bpparham@yahoo.com>
2017-07-13 14:52:31 -0500

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def contact
+  end
+
+  def home
+  end
+
+  def partners
+  end
+
+  def portfolio
+  end
+
+  def products
+  end
+
+  def services
+  end
+
+  def support
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/home'
+
+  get 'pages/partners'
+
+  get 'pages/portfolio'
+
+  get 'pages/products'
+
+  get 'pages/services'
+
+  get 'pages/support'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
